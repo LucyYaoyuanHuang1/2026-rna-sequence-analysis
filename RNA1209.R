680b551ab677bbb7057cf5014c174799d8e58b01
Lucy Huang <157838297+LucyYaoyuanHuang1@users.noreply.github.com>
2026-07-01 16:27:01 -0500

    7/1 Progress

--- RNA1209.R
+++ RNA1209.R
@@ -1,13 +1,14 @@
 #setup----
   #set working directory, for clarity, following Qi
   setwd("C:/Users/Lyhuang/Downloads/Data")
-  #load packages, magrittr for piping, dplyr and tibble for data manip, chm for heatmap, circ for heatmap colors
   library("tidyverse")
   library("ComplexHeatmap")
   library("circlize")
   library("ClassComparison")
   library("effectsize")
   library("magrittr")
+  library("qqplotr")
+  library("patchwork")
 
 #read in and prepare data----
   sample_data <- 
@@ -17,50 +18,71 @@
   #read in pool data, set the first column as rownames, as matrix, apply log(x + 1)
   pool_data <- 
     read.csv("pool.tpm_Gene.csv") %>%  
+    column_to_rownames(var = "X") %>%
+    t() %>% 
+    as.data.frame() %>% 
+    rownames_to_column(var = "Sample")
+  
+  log_pool_data <- 
+    read.csv("pool.tpm_Gene.csv") %>%  
     column_to_rownames(var = "X") %>% 
     mutate(across(where(is.double), ~add(.x, 1))) %>% 
     mutate(across(where(is.double), log2)) %>% 
     t() %>% 
     as.data.frame() %>% 
     rownames_to_column(var = "Sample")
-  #taking a look at base data distribution
-  long_pool = pool_data %>% pivot_longer(cols = !Sample, names_to = "gene", values_to = "values")
-  ggplot(long_pool, mapping = aes(x = values)) + geom_histogram(binwidth = .02) + 
-    labs(title = "ggplot(long_pool, mapping = aes(x = values)) + geom_histogram(binwidth = .02)")
-  ggplot(long_pool, mapping = aes(sample = values)) + geom_qq_line() +geom_qq() + 
   
-  a_data <- pool_data %>% 
-    semi_join(sample_data %>% 
-    filter(TimePoint == "48H"), by = join_by(Sample)) %>% 
-    column_to_rownames(var = "Sample") %>% 
-    t()
-  a_classes <- 
+#taking a look at base data distribution----
+  long_pool = pool_data %>% pivot_longer(cols = !Sample, names_to = "gene", values_to = "values") %>% filter(values >0) %>% slice(1:400)
+  log_long_pool = log_pool_data %>% pivot_longer(cols = !Sample, names_to = "gene", values_to = "values") %>% filter(values >0) %>% slice(1:400)
+  
+  hist <- ggplot(long_pool, mapping = aes(x = values)) + geom_histogram(bins = 100, fill = "green", alpha = .5) + 
+    xlab("ggplot(long_pool, mapping = aes(x = values)) + geom_histogram(bins = 100, fill = 'green', alpha = .5)") + 
+    labs(title = "Histogram, 1st 400 non-log adjusted values")
+  qqplot <- ggplot(long_pool, mapping = aes(sample = values)) + geom_qq_band(fill = 'green', alpha = .5) + stat_qq_point() + stat_qq_line() +
+      xlab("ggplot(long_pool, mapping = aes(sample = values)) + geom_qq_band(fill = 'green', alpha = .5) + stat_qq_point() + stat_qq_line()") + 
+      labs(title = "QQ Plot, 1st 400 non-log adjusted values")
+  log_hist <- ggplot(log_long_pool, mapping = aes(x = values)) + geom_histogram(bins = 100, fill = 'blue', alpha = .5) + 
+    xlab("ggplot(log_long_pool, mapping = aes(x = values)) + geom_histogram(bins = 100, fill = 'blue', alpha = .5)") +
+    labs(title = "Histogram, 1st 400 log adjusted values")
+  log_qqplot <- ggplot(log_long_pool, mapping = aes(sample = values)) + geom_qq_band(fill = 'blue', alpha = .5) + stat_qq_point() + stat_qq_line() + 
+    xlab("ggplot(log_long_pool, mapping = aes(sample = values)) + geom_qq_band(fill = 'blue', alpha = .5) + stat_qq_point() + stat_qq_line()") + 
+    labs(title = "QQ Plot, 1st 400 log adjusted values")
+
+  normality_figure <- (hist | qqplot) / (log_hist | log_qqplot)
+  
+  hist
+  qqplot
+  log_hist
+  log_qqplot
+  normality_figure
+  
+  shapiro.test(long_pool$values)
+  shapiro.test(log_long_pool$values)
+  
+#organizing data for ease of analysis----
+  make_classes <- function(grouping, attribute){
     sample_data %>% 
-    filter(TimePoint == "48H") %>% 
-    select(Group) %>% pull(Group) %>% 
-    factor()
-  b_data <- 
+      filter({{grouping}} == attribute) %>% 
+      select(Group) %>% 
+      pull(Group) %>% 
+      factor() %>% 
+      return()
+  }
+  a_classes <- make_classes(TimePoint, "48H")
+  b_classes <- make_classes(TimePoint, "72H")
+  c_classes <- make_classes(Treatment, "TTFields")
+  
+  make_data <- function(grouping, attribute) {
     pool_data %>% 
-    semi_join(sample_data %>% 
-    filter(TimePoint == "72H"), by = join_by(Sample)) %>% 
-    column_to_rownames(var = "Sample") %>% 
-    t()
-  b_classes <- 
-    sample_data %>% 
-    filter(TimePoint == "72H") %>% 
-    select(Group) %>% pull(Group)%>% 
-    factor()
-  c_data <- 
-    pool_data %>% 
-    semi_join(sample_data %>% 
-    filter(Treatment == "TTFields"), by = join_by(Sample)) %>% 
-    column_to_rownames(var = "Sample") %>% 
-    t()
-  c_classes <- 
-    sample_data %>% 
-    filter(Treatment == "TTFields") %>% 
-    select(Group) %>% pull(Group)%>% 
-    factor()
+      semi_join(sample_data %>% 
+        filter({{grouping}} == attribute), by = join_by(Sample)) %>% 
+      column_to_rownames(var = "Sample") %>% 
+      t()
+  }
+  a_data <- make_data(TimePoint, "48H")
+  b_data <- make_data(TimePoint, "72H")
+  c_data <- make_data(Treatment, "TTFields")
   
 #multi t-testing 48H----
   a_mttest <- MultiTtest(a_data, a_classes, na.rm = FALSE)
@@ -69,26 +91,45 @@
   a_cutoff = cutoffSignificant(a_bum, alpha = .05, by = "FDR")
   a_significant_genes <- a_data[selectSignificant(a_bum, by = "FDR"),] %>% na.omit()
   
-  #multi t-testing 72H----
+  #multi t-testing 72H
   b_mttest <- MultiTtest(b_data, b_classes, na.rm = TRUE)
   b_bum <- Bum(b_mttest@p.values)
   hist(b_bum, main = "72H")
   b_significant_genes <- b_data[selectSignificant(b_bum, alpha = .01, by = "FDR"),] %>% na.omit()
   
-  #multi t-testing TTFields----
+  #multi t-testing TTFields
   c_mttest <- MultiTtest(c_data, c_classes, na.rm = FALSE)
   c_bum <- Bum(c_mttest@p.values)
   hist(c_bum, main = "TTFields")
   c_significant_genes <- c_data[selectSignificant(c_bum, alpha = .01,by = "FDR"),] %>% na.omit()
 
-
+#Fold changes----
+  geo_mean <- function(attribute) {
+    log_pool_data %>% 
+    semi_join(sample_data %>% 
+                filter(Group == attribute), by = join_by(Sample)) %>% 
+    column_to_rownames("Sample") %>% 
+    t() %>% 
+    as.data.frame() %>% 
+    rownames_to_column(var = "Genes") %>% 
+    rowwise() %>% 
+    mutate(geo_mean = mean) %>% 
+    return ()
+  }
+  
+  geo_mean <- function(attribute) {
+    log_pool_data %>% 
+      semi_join(sample_data %>% 
+                  filter(Group == attribute), by = join_by(Sample)) %>% 
+      column_to_rownames("Sample") %>% 
+      rownames_to_column(var = "rowname") %>% 
+      pivot_longer(cols = !rowname) %>% 
+      return()
+  }
+  geo_mean_48H_ttfields <- geo_mean("TTFields 48H")
+  
+#Volcano maps----
 #heatmaps----
-  
-  #general and reference----
-  
-  #for ease of constant plot creation, use head as test data, and standardize for legibility of graph (+ and - values for diff colors)
-  #small_pool_data = a_significant_genes[1:10,] %>% standardize()
-  
   #set colors to be between red and blue and between -2 and 2
   color_function <- colorRamp2(c(-2, 0, 2), c("blue", "white", "red"))
 
@@ -109,15 +150,3 @@
       row_names_gp = gpar(fontsize = 2),
     )
     heatmap_48h_control_ttfields
-    
-#Fold changes and volcano maps----
-    #Fold changes----
-    a_fold_change = a_data$ / a_data
-    
-    #Volcano maps----
-    
-  
-#heatmap notes----
-#https://jokergoo.github.io/ComplexHeatmap-reference/book/a-single-heatmap.html
-  #general ComplexHeatmap info
-
